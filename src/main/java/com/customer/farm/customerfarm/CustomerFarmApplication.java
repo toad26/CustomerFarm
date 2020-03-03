d5aab64cd5eb51b3078d6d3e7e9380582694213c
marija <marija_toda@yahoo.com>
2020-03-03 13:05:29 +0100

    initial commit

---
+++ src/main/java/com/customer/farm/customerfarm/CustomerFarmApplication.java
@@ -0,0 +1,13 @@
+package com.customer.farm.customerfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerFarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerFarmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/customer/farm/customerfarm/CustomerFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customer.farm.customerfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerFarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
